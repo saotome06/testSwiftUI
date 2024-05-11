d0965f750b4fc2ae06026fae0922b2e33945d420
saotome06 <saotome03uhauha@gmail.com>
2024-05-11 16:20:03 +0900

    first commit

--- testSwiftUI/ContentView.swift
+++ testSwiftUI/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("早乙女")
         }
         .padding()
     }
