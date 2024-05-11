c3ea853d756c604e52947e65cd698c61d27ad26e
saotome06 <saotome03uhauha@gmail.com>
2024-05-11 16:18:52 +0900

    Initial Commit

---
+++ testSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testSwiftUI
+//
+//  Created by 早乙女琉真 on 2024/05/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testSwiftUI/testSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  testSwiftUIApp.swift
+//  testSwiftUI
+//
+//  Created by 早乙女琉真 on 2024/05/11.
+//
+
+import SwiftUI
+
+@main
+struct testSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
